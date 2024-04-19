a3025254287191abf226e7406b99cdfed7cc66dc
Zubair wani <48851176+ZubairWani@users.noreply.github.com>
2024-04-19 17:03:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-#
+ function calculate() {
+            var a = parseFloat(document.getElementById('totalCholesterol').value);
+            var b = parseFloat(document.getElementById('triglycerides').value);
+            var c = parseFloat(document.getElementById('hdl').value);
+
+            var vldl = b / 5;
+            var ldl = a - c - vldl;
+            var nonHDL = a - c;
+            var cholHDL = a / c;
+            var ldlHDL = ldl / c;
+
+            var results = "<p>LDL Cholesterol : " + ldl.toFixed(2) + " mg/dL</p>";
+            results += "<p>VLDL Cholesterol : " + vldl.toFixed(2) + " mg/dL</p>";
+            results += "<p>Non HDL Cholesterol : " + nonHDL.toFixed(2) + " mg/dL</p>";
+            results += "<p>Cholesterol/HDL Ratio : " + cholHDL.toFixed(2) + "</p>";
+            results += "<p>LDL/HDL Ratio : " + ldlHDL.toFixed(2) + "</p>";  
+            
+
+            document.getElementById('results').innerHTML = results;
+        }
